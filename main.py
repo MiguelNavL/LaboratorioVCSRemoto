6b633b7ec8369fa442380b90cf90e7ee558f0611
Jossie Esteban Rodríguez Salazar <jossie.rodriguez@losmoviles.com.co>
2019-02-22 16:00:27 -0500

    Solucion

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ import math
 # Ejercicio 1
 
 a = 1
-b = 2
-c = 3
+b = 8
+c = 4
+
 d = 4
 
 multiplicacion = a*b*c
@@ -20,3 +21,23 @@ raizB = math.sqrt(4)
 
 print('El cuadrado de B = ', cuadradoB)
 print('La raiz de D = ', raizB)
+
+# Ejercicio 3
+
+requisito = cuadradoB - 4*a*c
+
+if requisito > 0:
+    d = math.sqrt(cuadradoB)
+    
+    print(d)
+
+    if d > 0:
+        x1 = -b + math.sqrt(d) / 2*a
+        x2 = -b - math.sqrt(d) / 2*a
+    else:
+        if d == 0:
+            x1 = -b / 2*a
+
+    print('La solucion es = ', x1, x2)
+else:
+    print('No se puede calacular')
