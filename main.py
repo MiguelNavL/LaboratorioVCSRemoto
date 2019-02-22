b1e2ac7843e1f08e7471c1f86e8275e4af48fc02
Jossie Esteban Rodríguez Salazar <jossie.rodriguez@losmoviles.com.co>
2019-02-22 15:39:12 -0500

    Mi primera modificacion

--- main.py
+++ main.py
@@ -1,11 +1,22 @@
+import math
+
 # Ejercicio 1
 
 a = 1
 b = 2
 c = 3
+d = 4
 
 multiplicacion = a*b*c
 doble = 2*a
 
 print('Multiplicacion = ', multiplicacion)
 print('Doble A = ', doble)
+
+# Ejercicio 2
+
+cuadradoB = b**2
+raizB = math.sqrt(4)
+
+print('El cuadrado de B = ', cuadradoB)
+print('La raiz de D = ', raizB)
